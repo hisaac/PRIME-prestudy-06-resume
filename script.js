9d063a65dac4858411f78d383d860d574165e5bf
Isaac Halvorson <hello@hisaac.net>
2016-10-16 17:56:12 -0500

    sync repos

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function(){
   $('#button').on('click', function(){
-  	$('#button').css({'display': 'hidden'});
-  	$('#content').css({'display': 'block'});
+    
   });
   $('#button').hover(function(){
     $(this).css('cursor', 'pointer');
