375b2288c02bc62bfb7f0f7a5a5737615a4919af
Isaac Halvorson <hello@hisaac.net>
2016-10-16 18:04:32 -0500

    import jQuery from Google CDN, and finish tweaking scripts and layout

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function(){
   $('#button').on('click', function(){
-    
+		$('#content').css({'display': 'block'});
+    $('#button').css({'display': 'none'});
   });
   $('#button').hover(function(){
     $(this).css('cursor', 'pointer');
